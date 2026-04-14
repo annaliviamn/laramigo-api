328713410c2f3d63ba340a3152fb0baec6cbd1b5
Anna Livia <annaliviamaciel@gmail.com>
2026-04-14 18:22:39 -0300

    fix db for better-sqlite3

--- src/database.js
+++ src/database.js
@@ -6,17 +6,17 @@ const dbPath = path.resolve(__dirname, "pets.db");
 const db = new Database(dbPath);
 
 db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS pets (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nome TEXT,
-      idade INTEGER,
-      porte TEXT,
-      descricao TEXT,
-      status TEXT,
-      imagem TEXT
-    )
-  `);
+  db.prepare(`
+  CREATE TABLE IF NOT EXISTS pets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    idade INTEGER,
+    porte TEXT,
+    descricao TEXT,
+    status TEXT,
+    imagem TEXT
+  )
+`).run();
 });
 
 module.exports = db;
